7137b9e1fa89ece8131c4c31053a29c807c831e7
Shashank <sashankviswanadha@gmail.com>
2016-07-06 22:55:03 +0530

    Fixed a couple of bugs

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 class Config():
-    path = 'your path here' # EXAMPLE : /home/shashank
+    path = 'set your path here' # EXAMPLE : /home/shashank
 
 
 # You only set this the first time you use this script. Try not to change this once you set it. # If you don't change the path you will get only the newly added files from moodle as you
